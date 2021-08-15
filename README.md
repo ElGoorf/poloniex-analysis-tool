f0263b6c80b7f4b6d6730ca5a61970636f358f88
Hussein Duvigneau <1381698+ElGoorf@users.noreply.github.com>
2021-08-15 17:11:46 +0200

    v1

---
+++ README.md
@@ -0,0 +1,15 @@
+# Poloniex Analysis Tool
+
+Summarises all your open sell orders in a single table.
+
+## Disclaimer
+
+This is a third-party, unofficial tool with no affiliation to Poloniex. This tool is open-source, provided for use at user's discretion, without warranty or guarantee!
+
+## How to set up
+
+1. Install Node, and install dependencies with yarn or npm as you prefer.
+2. Copy or rename `credentials.sample.json` to `credentials.json`.
+3. Create a Poloniex APi key: https://poloniex.com/apiKeys. For own safety, leave "Enable Trading" and "Enable Withdrawals" unchecked, as they are not required.
+4. Enter your Poloniex API key details into `credentials.json`.
+5. `node index.js`.
---
+++ index.js
@@ -0,0 +1,56 @@
+import Poloniex from "poloniex-api-node";
+import { readFile } from 'fs/promises';
+
+async function run() {
+    const credentials = JSON.parse(
+        await readFile(
+            new URL('./credentials.json', import.meta.url)
+        )
+    );
+
+    const poloniex = new Poloniex (credentials.key, credentials.secret);
+    poloniex.returnOpenOrders('ALL', function (err, orders) {
+        if (err) {
+            throw new Error(err)
+        }
+
+        poloniex.returnTicker((tickerErr, ticker) => {
+            if (tickerErr) {
+                console.log(tickerErr.message);
+            } else {
+
+
+                const actualOrders = Object.entries(orders).filter(([pair, ordersForPair]) => ordersForPair.length > 0);
+
+                let table = [['pair', 'total order sell', 'total sell quantity', 'av. sell price', 'lowest sell price', 'current value', '24hr', 'optimism', 'optimism at lowest']];
+                actualOrders.forEach(([pair, ordersForPair]) => {
+                    const summary = ordersForPair.reduce((acc, order) => {
+                        if (order.type === 'sell' && order.margin === 0) {
+                            acc.totalOrderValue += parseFloat(order.total);
+                            acc.totalOrderQuantity += parseFloat(order.startingAmount);
+
+                            acc.avOrderSellPrice = acc.totalOrderValue/acc.totalOrderQuantity;
+
+                            if (acc.lowestPrice > order.rate) {
+                                acc.lowestPrice = order.rate;
+                            }
+                        }
+                        return acc;
+                    }, { totalOrderValue: 0, totalOrderQuantity: 0, avOrderSellPrice: 0, lowestPrice: Number.MAX_SAFE_INTEGER });
+
+                    table.push([
+                        pair,
+                        ...Object.values(summary),
+                        ticker[pair].last,
+                        `${parseFloat(ticker[pair].percentChange * 100).toFixed(2)} %`, summary.avOrderSellPrice / ticker[pair].last, summary.lowestPrice / ticker[pair].last,
+                    ])
+                });
+                console.table(table)
+
+
+            }
+        });
+    });
+}
+
+run();
